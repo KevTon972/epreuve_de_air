e6fc93a2ee9fca1d2d3ab02b7c0ad25b5c25e8fa
KevTon972 <kevin.clerima@outlook.fr>
2022-03-28 09:22:03 +0200

    2e exo

---
+++ air01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#Créez un programme qui découpe une chaîne de caractères en tableau en fonction du séparateur donné en 2e argument.
+import sys
+import re
+
+def ma_fonction(arg):
+    
+    liste = []
+
+    for i in arg2:
+        i = re.sub('[\W]', '', i) 
+        liste.append(i)
+
+    indx = liste.index(arg)
+
+
+    print(" ".join(liste[0:indx]))
+    print(" " + " ".join(liste[indx+1:]))
+
+arg = sys.argv[1:]
+arg2 = arg[0:-1]
+arg= re.sub('[\W]', '', arg[-1]) 
+
+ma_fonction(arg)
